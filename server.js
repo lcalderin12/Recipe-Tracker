c6e91c2f15c895330e8a0195c17e560ffb57397f
Eric Johnson <ericjohnson0216@icloud.com>
2020-11-11 18:52:44 -0500

    new_updates_2

--- server.js
+++ server.js
@@ -1,29 +1,49 @@
 var express = require("express");
-
-// Sets up the Express App
-// =============================================================
+var PORT = process.env.PORT || 3030;
 var app = express();
-var PORT = process.env.PORT || 8080;
 
-// Requiring our models for syncing
-var db = require("./models");
+// Serve static content for the app from the "views" directory in the application directory.
+app.use(express.static("public"));
 
-// Sets up the Express app to handle data parsing
+// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static directory
+// SETUP HANDLEBARS
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+require("./routes/html-routes")(app);
+
+// app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});var express = require("express");
+var PORT = process.env.PORT || 3030;
+var app = express();
+
+// Serve static content for the app from the "views" directory in the application directory.
 app.use(express.static("public"));
 
-// Routes
-// =============================================================
-require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+// SETUP HANDLEBARS
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+require("./routes/html-routes")(app);
+
+// app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
 });
