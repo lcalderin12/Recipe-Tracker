1302eb3e54e92971cfff2afb2d9f8b8dcbc06346
Eric Johnson <ericjohnson0216@icloud.com>
2020-11-11 18:41:03 -0500

    new_updates

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,88 +1,85 @@
-// *********************************************************************************
 // api-routes.js - this file offers a set of routes for displaying and saving data to the db
-// *********************************************************************************
+// *******************************************************************************
 
-// Dependencies
-// =============================================================
-
-// Requiring our Todo model
+// Requiring our Recipe model
 var db = require("../models");
 
 // Routes
-// =============================================================
+
 module.exports = function(app) {
 
-  // GET route for getting all of the posts
-    app.get("/recipes", function(req, res) {
-        db.Recipe.findAll({}).then(function(result) {
+  // GET route for getting all of the posts and return them to the user with res.json
+    app.get("/api/all", function(req, res) {
+      db.Recipes.findAll({}).then(function(result) {
         res.json(result);
         });
     });
-
-  // Get route for returning posts of a specific category where the category is equal to req.params.category,
-//   app.get("/api/posts/category/:category", function(req, res) {
-//     db.Post.findAll ({
-//         where: {
-//             category: req.params.category,
-//         } 
-//     }).then(function (result){
-//         res.json(result);
-//     });
-
-//   });
-
-  // Get route for retrieving a single recipe where the id is equal to req.params.id.
-  app.get("/recipes/:id", function(req, res) {
-    db.Recipes.findOne ({
+    
+      // Get route for retrieving a single recipe where the id is equal to req.params.id
+    app.get("/api/recipes/:id", function(req, res) {
+      db.Recipe.findOne ({
         where: {
             id: req.params.id,
         } 
-    }).then(function (result){
-        res.json(result);
-    });
+      }).then(function (result){
+          res.json(result);
+      });
      
-  });
+    });
 
-  // POST route for saving a new recipe post using req.body,
-  app.post("/recipes", function(req, res) {
-     db.Recipe.create ({
+    // POST route for saving a new recipe for creating a recipe using req.content
+    app.post("/api/new", function(req, res) {
+      
+      db.Recipe.create ({
         name: req.body.name,
-        ingredient: req.body.ingredient,
+        ingredients: req.body.ingredients,
         category: req.body.category,
         content: req.body.content
            
-    }).then(function (result){
+      }).then(function (result){
         res.json(result);
-    })
-  });
-
-  // DELETE route for deleting recipe post where the id is equal to req.params.id,
-  app.delete("/recipes/:id", function(req, res) {
-     db.Recipe.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(result) {
-      res.json(result);
+      })
     });
-  });
 
-  // PUT route for updating recipe posts using the values in req.body, where the id is equal to req.body.id
-  app.put("/recipes", function(req, res) {
-    db.Recipe.update({
-        name: req.body.name,
-        ingredient: req.body.ingredient,
-        category: req.body.category,
-        content: req.body.content
+
+    app.put("/recipes", function(req, res) {
       
-    }, {
-      where: {
-        id: req.body.id
-      }
-    }).then(function(result) {
-      res.json(result);
+      db.Recipe.update({
+          name: req.body.name,
+          ingredients: req.body.ingredients,
+          category: req.body.category,
+          content: req.body.content
+      
+      }, {
+        where: {
+          id: req.body.id
+        }
+      }).then(function(result) {
+        res.json(result);
+      });
     });
 
-  });
+    // DELETE route for deleting recipes where the id is equal to req.params.id,
+    app.delete("/recipes/:id", function(req, res) {
+      db.Recipe.destroy({
+        where: {
+          id: req.params.id
+        }
+      }).then(function(result) {
+        res.json(result);
+      });
+    });
+
+ // Get route for returning posts of a specific category ind all posts where the category is equal to req.params.category
+  // app.get("/recipes/category/:category", function(req, res) {
+  //   db.Recipe.findAll ({
+  //       where: {
+  //           category: req.params.category,
+  //       } 
+  //   }).then(function (result){
+  //       res.json(result);
+  //   });
+
+  // });
 
 };
